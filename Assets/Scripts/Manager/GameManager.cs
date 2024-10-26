3c7bdb31ac32d70f48e04d60cb8292aba03dd84a
Xwilarg <xwilarg@protonmail.com>
2024-10-26 19:15:53 +0200

    Add jam registering

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,5 +4,31 @@ namespace OneHourGameJam.Manager
 {
     public class GameManager : MonoBehaviour
     {
+        public static GameManager Instance { private set; get; }
+
+        [SerializeField]
+        private SpriteRenderer _bg;
+
+        [SerializeField]
+        private Sprite[] _sprites;
+
+        private int _counter;
+        private int _max;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void Register()
+        {
+            _counter++;
+            _max++;
+        }
+
+        public void Remove()
+        {
+            _counter--;
+        }
     }
 }
---
+++ Assets/Scripts/Map/JamPart.cs
@@ -0,0 +1,13 @@
+﻿using OneHourGameJam.Manager;
+using UnityEngine;
+
+namespace OneHourGameJam.Map
+{
+    public class JamPart : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            GameManager.Instance.Remove();
+        }
+    }
+}
