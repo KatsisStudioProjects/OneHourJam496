6307841a061fbfc1d53acd251d072f748b2aa9a2
Xwilarg <xwilarg@protonmail.com>
2024-10-26 19:26:04 +0200

    Add explosion

--- Assets/Scripts/Map/JamPart.cs
+++ Assets/Scripts/Map/JamPart.cs
@@ -1,13 +1,40 @@
 ﻿using OneHourGameJam.Manager;
+using System.Collections;
 using UnityEngine;
 
 namespace OneHourGameJam.Map
 {
     public class JamPart : MonoBehaviour
     {
+        [SerializeField]
+        private Sprite _explosion;
+
+        private Collider2D _coll;
+        private SpriteRenderer _sr;
+
+        private void Awake()
+        {
+            _coll = GetComponent<Collider2D>();
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        private void Start()
+        {
+            GameManager.Instance.Register();
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             GameManager.Instance.Remove();
+            StartCoroutine(Explode());
+        }
+
+        private IEnumerator Explode()
+        {
+            _coll.enabled = false;
+            _sr.sprite = _explosion;
+            yield return new WaitForSeconds(.5f);
+            Destroy(gameObject);
         }
     }
 }
