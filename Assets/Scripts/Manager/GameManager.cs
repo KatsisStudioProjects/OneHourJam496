a3a023785122e06b94c4daf5492488bcc4775ee3
Xwilarg <xwilarg@protonmail.com>
2024-10-26 19:31:38 +0200

    Add player lock

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -32,5 +32,17 @@ namespace OneHourGameJam.Manager
             var prog = (float)_counter * (_sprites.Length - 1) / _max;
             _bg.sprite = _sprites[Mathf.FloorToInt(prog)];
         }
+
+
+        // http://answers.unity.com/answers/502236/view.html
+        public static Bounds CalculateBounds(Camera cam)
+        {
+            float screenAspect = Screen.width / (float)Screen.height;
+            float cameraHeight = cam.orthographicSize * 2;
+            Bounds bounds = new(
+                cam.transform.position,
+                new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+            return bounds;
+        }
     }
 }
